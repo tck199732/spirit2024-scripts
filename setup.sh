d2c516c760c359074b549640a3d94cbb015fd838
tck199732 <ctb8135@wmich.edu>
2025-04-14 13:07:54 -0400

    🔨 update configurtion script for local machine

--- setup.sh
+++ setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# NO NEED TO RUN THIS SCRIPT IF YOU ARE ON RIKEN MACHINE WITH ANAROOTLOGIN COMMAND
+# NO NEED TO RUN THIS SCRIPT IF YOU ARE ON RIKEN MACHINE WITH ANAROOTLOGIN COMMAND (s063)
 # FOR LOCAL USE ONLY
 
+# deactivate conda environment as it may contains ROOT installation
 while [ ! -z $CONDA_PREFIX ]; do
     conda deactivate
 done
 
-source /home/kin/software/root/bin/thisroot.sh
-export TARTSYS=/home/kin/Desktop/spirit2024-scripts/src/anaroot/install
+# CHANGE TO YOUR ROOT INSTALLATION PATH
+source ~/software/root/bin/thisroot.sh
+# CHANGE TO YOUR ANAROOT INSTALLATION PATH
+export TARTSYS=$(pwd)/src/anaroot/install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARTSYS/lib
-
-#For MINOS
-export LD_LIBRARY_PATH=$HOME/exp/minos/install/pentium-linux-release/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/exp/minos/liboffline:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$HOME/exp/minos/install/pentium-linux-release/lib/pkgconfig
-export XMLDIPOLESH=$HOME/epics/xmldipole.sh
