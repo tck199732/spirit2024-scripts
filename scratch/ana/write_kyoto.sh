240974492567f43acebf4e188830c38f96524d43
tck199732 <ctb8135@wmich.edu>
2025-04-14 12:41:40 -0400

    🔥 remove log and result files

--- scratch/ana/write_kyoto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#for idx in $(seq 1148 1153); do 
-for idx in $(seq 1263 1265); do 
-    root -l -b -q "macros/WriteKyotoHits.C(\"analyzed/data$idx.root\", \"result/kyotoHits/data$idx.txt\")"
-done
